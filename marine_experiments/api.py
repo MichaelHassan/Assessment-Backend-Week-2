6cf915f868fbaa0327669603f5b50aa567226045
MichaelHassan-SigmaLabs <trainee.michael.hassan@sigmalabs.co.uk>
2024-08-09 17:29:08 +0100

    half of task 5

--- marine_experiments/api.py
+++ marine_experiments/api.py
@@ -53,13 +53,11 @@ def subjects():
     return subjects, 200
 
 
-@app.route("/experiment", methods=["GET"])
+@app.route("/experiment", methods=["GET", "POST"])
 def experiment():
     """Returns an subjects"""
 
-    where_case = ""
-
-    query = """
+    query_none = """
             SELECT
             e.experiment_id, e.subject_id, sp.species_name AS species , e.experiment_date,
             et.type_name AS experiment_type , (e.score/et.max_score * 100) AS score
@@ -70,43 +68,165 @@ def experiment():
             ON et.experiment_type_id = e.experiment_type_id
             JOIN species AS sp
             ON s.species_id = sp.species_id
+            ORDER BY e.experiment_date DESC ;
             """
 
-    args = request.args.to_dict()
+    query_both = """
+            SELECT
+            e.experiment_id, e.subject_id, sp.species_name AS species , e.experiment_date,
+            et.type_name AS experiment_type , (e.score/et.max_score * 100) AS score
+            FROM experiment AS e
+            JOIN subject AS s
+            ON s.subject_id = e.subject_id
+            JOIN experiment_type AS et
+            ON et.experiment_type_id = e.experiment_type_id
+            JOIN species AS sp
+            ON s.species_id = sp.species_id
+            WHERE et.type_name ILIKE %s AND (e.score/et.max_score * 100) > %s
+            ORDER BY e.experiment_date DESC ;
+            """
 
-    if args.get("type") and args.get("score_over"):
-        if args.get("type") in ["intelligence", "obedience", "aggression"] and int(args.get("score_over")) >= 0 and int(args.get("score_over")) <= 100:
-            where_case = f"""WHERE experiment_type = {args.get("type")} AND score > {
-                args.get("score_over")} """
-        else:
-            return {"error": True, "message": f"Invalid value for {args.get("type")} parameter"}, 400
-    elif args.get("type"):
-        if args.get("type") in ["intelligence", "obedience", "aggression"]:
-            where_case = f"""WHERE experiment_type = {args.get("type")}"""
-        else:
-            return {"error": True, "message": f"Invalid value for {args.get("type")} parameter"}, 400
-    elif args.get("score_over"):
-        if int(args.get("score_over")) >= 0 and int(args.get("score_over")) <= 100:
-            where_case = f"""WHERE score > {args.get("score_over")} """
-        else:
-            return {"error": True, "message": f"Invalid value for {args.get("score_over")} parameter"}, 400
+    query_type = """
+            SELECT
+            e.experiment_id, e.subject_id, sp.species_name AS species , e.experiment_date,
+            et.type_name AS experiment_type , (e.score/et.max_score * 100) AS score
+            FROM experiment AS e
+            JOIN subject AS s
+            ON s.subject_id = e.subject_id
+            JOIN experiment_type AS et
+            ON et.experiment_type_id = e.experiment_type_id
+            JOIN species AS sp
+            ON s.species_id = sp.species_id
+            WHERE et.type_name ILIKE %s
+            ORDER BY e.experiment_date DESC ;
+            """
+    query_score = """
+            SELECT
+            e.experiment_id, e.subject_id, sp.species_name AS species , e.experiment_date,
+            et.type_name AS experiment_type , (e.score/et.max_score * 100) AS score
+            FROM experiment AS e
+            JOIN subject AS s
+            ON s.subject_id = e.subject_id
+            JOIN experiment_type AS et
+            ON et.experiment_type_id = e.experiment_type_id
+            JOIN species AS sp
+            ON s.species_id = sp.species_id
+            WHERE (e.score/et.max_score * 100) > %s
+            ORDER BY e.experiment_date DESC ;
+            """
 
-    query += where_case
-    query += "\nORDER BY e.experiment_date DESC ;"
+    if request.method == "GET":
 
-    print(query)
+        args = request.args.to_dict()
+
+        if args.get("type") and args.get("score_over"):
+            try:
+                int(args.get("score_over"))
+            except TypeError:
+                return {"error": True, "error": "Invalid value for 'score_over' parameter"}, 400
+
+            if args.get("type").lower() in ["intelligence", "obedience", "aggression"] and int(args.get("score_over")) >= 0 and int(args.get("score_over")) <= 100:
+                with conn.cursor() as cur:
+                    cur.execute(query_both, (args.get(
+                        "type"), int(args.get("score_over"))))
+                    data = cur.fetchall()
+            else:
+                return {"error": True, "error": f"Invalid value for 'type' parameter"}, 400
+        elif args.get("type"):
+            if args.get("type").lower() in ["intelligence", "obedience", "aggression"]:
+                with conn.cursor() as cur:
+                    cur.execute(query_type, (args.get("type"),))
+                    data = cur.fetchall()
+            else:
+                return {"error": True, "error": "Invalid value for 'type' parameter"}, 400
+        elif args.get("score_over"):
+            try:
+                int(args.get("score_over"))
+            except Exception:
+                return {"error": True, "error": "Invalid value for 'score_over' parameter"}, 400
+            if int(args.get("score_over")) >= 0 and int(args.get("score_over")) <= 100:
+
+                print(args.get("score_over"))
+
+                with conn.cursor() as cur:
+                    cur.execute(query_score, (args.get("score_over"),))
+                    data = cur.fetchall()
+            else:
+                return {"error": True, "error": "Invalid value for 'score_over' parameter"}, 400
+        else:
+            with conn.cursor() as cur:
+                cur.execute(query_none)
+                data = cur.fetchall()
+
+        experiments = list(data)
+
+        for experiment in experiments:
+            experiment["experiment_date"] = str(experiment["experiment_date"])
+            experiment["score"] = f"{float(experiment["score"]):0.2f}%"
+
+        return experiments, 200
+
+    data = request.json
+
+    if data.get("subject_id") and data.get("experiment_type") and data.get("score"):
+        try:
+            if type(data.get("subject_id")) != int:
+                raise ValueError
+            if not int(data.get("subject_id")) > 0:
+                raise ValueError
+        except ValueError:
+            return {'error': "Invalid value for 'subject_id' parameter."}, 400
+
+        try:
+            if data.get("experiment_type").lower() not in ["intelligence", "obedience", "aggression"]:
+                raise ValueError
+        except Exception:
+            return {"error": "Invalid value for 'experiment_type' parameter."}, 400
+
+        try:
+            int(data.get("score"))
+            if type(data.get("score")) != int:
+                raise ValueError
+            if not int(data.get("score")) > 0:
+                raise ValueError
+        except Exception:
+            return {'error': "Invalid value for 'score' parameter."}, 400
+
+    if not data.get("subject_id"):
+        return {'error': "Request missing key 'subject_id'."}, 400
+    elif not data.get("experiment_type"):
+        return {'error': "Request missing key 'experiment_type'."}, 400
+    elif not data.get("score"):
+        return {'error': "Request missing key 'score'."}, 400
+
+    return {}, 201
+
+
+@ app.route("/experiment/<int:id>", methods=["DELETE"])
+def endpoint_get_movie(id: int):
+
+    query = """DELETE FROM experiment
+                    WHERE experiment_id = %s ;
+                    """
+
+    query_search = """SELECT experiment_id,experiment_date FROM experiment
+                    WHERE experiment_id = %s ;"""
 
     with conn.cursor() as cur:
-        cur.execute(query)
-        data = cur.fetchall()
+        cur.execute(query_search, (id,))
+        data = list(cur.fetchall())
 
-    experiments = list(data)
+    if not data:
+        return {"error": f"Unable to locate experiment with ID {id}."}, 404
 
-    for experiment in experiments:
+    with conn.cursor() as cur:
+        cur.execute(query, (id,))
+        conn.commit()
+
+    for experiment in data:
         experiment["experiment_date"] = str(experiment["experiment_date"])
-        experiment["score"] = f"{float(experiment["score"]):0.2f}%"
 
-    return experiments, 200
+    return data[0], 200
 
 
 if __name__ == "__main__":
